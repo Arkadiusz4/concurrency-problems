a3138654f75ed9d568f1cea140cecb88c674f900
Arkadiusz4 <arekmika4@gmail.com>
2024-02-19 00:14:11 +0100

    Create main.go

---
+++ producer-consumer/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/fatih/color"
+	"math/rand"
+	"time"
+)
+
+const NumberOfPizzas = 10
+
+var pizzasMade, pizzasFailed, total int
+
+type PizzaOrder struct {
+	pizzaNumber int
+	message     string
+	success     bool
+}
+
+type Producer struct {
+	data chan PizzaOrder
+	quit chan chan error
+}
+
+func (p *Producer) Close() error {
+	ch := make(chan error)
+	p.quit <- ch
+	return <-ch
+}
+
+func pizzeria(pizzaMaker *Producer) {
+	for {
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	color.Cyan("The Pizzeria is open for business!")
+	color.Cyan("----------------------------------")
+
+	pizzaJob := &Producer{
+		data: make(chan PizzaOrder),
+		quit: make(chan chan error),
+	}
+
+	go pizzeria(pizzaJob)
+
+}
