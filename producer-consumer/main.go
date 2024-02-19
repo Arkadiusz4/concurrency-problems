fd7eb3afd7fdf05ba2ee73d395371c90f9488991
Arkadiusz4 <arekmika4@gmail.com>
2024-02-19 17:20:00 +0100

    Update producer

--- producer-consumer/main.go
+++ producer-consumer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/fatih/color"
 	"math/rand"
 	"time"
@@ -27,8 +28,70 @@ func (p *Producer) Close() error {
 	return <-ch
 }
 
+func makePizza(pizzaNumber int) *PizzaOrder {
+	pizzaNumber++
+
+	if pizzaNumber <= NumberOfPizzas {
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Received order #%d!\n", pizzaNumber)
+
+		rnd := rand.Intn(12) + 1
+		msg := ""
+		success := false
+
+		if rnd < 5 {
+			pizzasFailed++
+		} else {
+			pizzasMade++
+		}
+
+		total++
+
+		fmt.Printf("Making pizza #%d. It will take %d seconds...\n", pizzaNumber, delay)
+
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("*** We ran out of ingredient for pizza #%d", pizzaNumber)
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("*** The cook quit while making pizza #%d", pizzaNumber)
+		} else {
+			success = true
+			msg = fmt.Sprintf("Pizza order #%d is ready", pizzaNumber)
+		}
+
+		p := PizzaOrder{
+			pizzaNumber: pizzaNumber,
+			message:     msg,
+			success:     success,
+		}
+
+		return &p
+	}
+
+	return &PizzaOrder{
+		pizzaNumber: pizzaNumber,
+	}
+}
+
 func pizzeria(pizzaMaker *Producer) {
+
+	var i = 0
+
 	for {
+		currentPizza := makePizza(i)
+
+		if currentPizza != nil {
+			i = currentPizza.pizzaNumber
+			select {
+			case pizzaMaker.data <- *currentPizza:
+
+			case quickChan := <-pizzaMaker.quit:
+				close(pizzaMaker.data)
+				close(quickChan)
+				return
+			}
+		}
 	}
 }
 
